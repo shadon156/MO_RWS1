f1959bd6d7ec218f5c0eb5b2bfa0aa5aeea03b00
shadon-156 <21136033@student.hhs.nl>
2024-05-28 12:25:47 +0200

    assets and home

--- my-project/src/routes/+page.svelte
+++ my-project/src/routes/+page.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body class="bg-gradient-to-t from-secondary">
+    <div class="">
+        <div class="flex justify-between">
+            <div class="flex justify-start">
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <div><img src="src/assets/image 3.png" /></div>
+                <div class="text-4xl p-2 pt-5 font-bold">
+                    Rijkswaterstaat experience
+                </div>
+            </div>
+            <div class="text-xl font-bold p-5">Contact | Interantional</div>
+        </div>
+    </div>
+    <div class="text-3xl font-bold pt-44 pl-3 pb-4">
+        Rijkswaterstaat Experience
+    </div>
+    <div class="flex">
+        <div class="text-2xl pl-3 font-bold">Experience</div>
+        <div class="text-lg text-pretty font-semibold pt-1.5 pl-1">
+            hoe Rijkswaterstaat aan de waterveiligheid werkt.
+        </div>
+    </div>
+    <div class="flex pl-16 pt-6">
+        <button
+            type="button"
+            class="text-lg bg-secondary rounded-lg text-white hover:drop-shadow-md w-32 h-11"
+        >
+            Meer info
+        </button>
+        <div class="pl-3">
+            <button
+                type="button"
+                class="text-lg bg-primary rounded-lg hover:drop-shadow-lg text-white w-44 h-11"
+            >
+                Begin
+            </button>
+        </div>
+    </div>
+    <div class="absolute inset-y-60 right-0">
+        <img src="src/assets/logo.png" alt="">
+    </div>
+    <div class="flex justify-center font-bold text-2xl pt-96">Wat is Rijkswaterstaat Experience ?</div>
+    <div class="flex justify-evenly columns-3 p-10">
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img class="w-64 h-96" src="src/assets/image 5.png" />
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img class="w-64 h-96" src="src/assets/image 6.png" />
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img class="w-64 h-96" src="src/assets/image 7.png" />
+    </div>
+    <div class="flex justify-center">
+        <div class="text-center font-bold pl-32 pr-32 pt-3">
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
+            in laying out print, graphic or web designs. The passage is
+            attributed to an unknown typesetter in the 15th century who is
+            thought to have scrambled parts of Cicero's De Finibus Bonorum et
+            Malorum for use in a type specimen book
+        </div>
+    </div>
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img class="w-screen mt-0 p-0" src="src/assets/waves.png" />
+    <body class="bg-water flex justify-evenly pt-44">
+        <div class="flex-col">
+            <div class="text-3xl font-bold text-white pt-10">
+                Wat is het doel van het
+            </div>
+            <div class="text-3xl font-bold text-white pb-5">
+                Rijkswaterstaat Experience?
+            </div>
+            <div class="text-white w-96 pb-32">
+                Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+                used in laying out print, graphic or web designs. The passage is
+                attributed to an unknown typesetter in the 15th century who is
+                thought to have scrambled parts of Cicero's De Finibus Bonorum
+                et Malorum for use in a type specimen book
+            </div>
+        </div>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img class="w-96" src="src/assets/image 1.png" />
+    </body>
+    <body class=" bg-water">
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <img class="bg-local w-screen" src="src/assets/sand.png">
+        <div class="flex justify-evenly">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </div>
+    </body>
+    
+</body>
