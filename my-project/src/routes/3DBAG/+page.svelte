fc4457f92e61a93a503c0b6cfa05c7778bd3db68
shadon-156 <21136033@student.hhs.nl>
2024-06-04 21:27:03 +0200

    three test

---
+++ my-project/src/routes/3DBAG/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import ThreeDMap from '$lib/ThreeDMap.svelte';
+  </script>
+  
+  <style>
+    main {
+      @apply h-screen;
+    }
+  </style>
+  
+  <main>
+    <ThreeDMap />
+  </main>
+  
